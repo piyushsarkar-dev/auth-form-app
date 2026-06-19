b6e3be3b3ddba3e79c91dacb5a2e307cd7921c56
Piyush Sarkar <taramaelectricalsgroup@gmail.com>
2026-06-19 22:00:40 +0530

    The Register Logic Done

--- src/components/customui/Login.tsx
+++ src/components/customui/Login.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 const Login = () => {
-  return <>Enter</>;
+  return <></>;
 };
 
 export default Login;
--- src/components/customui/Register.tsx
+++ src/components/customui/Register.tsx
@@ -1,5 +1,88 @@
+"use client";
+
+import { registerSchema, RegisterSchemaType } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "../shadcnui/button";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+
 const Register = () => {
-  return <>Enter</>;
+  const { handleSubmit, control } = useForm({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+    },
+    mode: "all",
+  });
+  const handleRegister = async (register: RegisterSchemaType) => {
+    await new Promise((Resolver) => setTimeout(Resolver, 1500));
+    console.log(register);
+  };
+
+  return (
+    <>
+      <form
+        className="grid gap-2"
+        noValidate
+        onSubmit={handleSubmit(handleRegister)}>
+        <Controller
+          name="fullName"
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={field.name}>Full Name</FieldLabel>
+              <Input
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+                placeholder="Enter Your Full Name"
+                autoComplete="name"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="email"
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={field.name}>Email Adress</FieldLabel>
+              <Input
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+                placeholder="Enter Your Email Adress"
+                autoComplete="email"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={field.name}>Passowrd</FieldLabel>
+              <Input
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+                placeholder="Generate Your Password"
+                autoComplete="new-password"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
